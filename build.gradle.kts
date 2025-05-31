c87e9f50c97a52abd692cc97696ced788165ad0d
AntepRohman <154037519+AntepRohman@users.noreply.github.com>
2025-05-31 11:43:23 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.google.firebase.crashlytics) apply false
 }
 
-plugins {
-alias(libs.plugins.google.gms.google.services) apply false
-alias(libs.plugins.google.firebase.crashlytics) apply false
-}
